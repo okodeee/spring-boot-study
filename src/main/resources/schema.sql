81b835abd190c89b6afa848e6fd0024d4262da03
okodee <okodee@naver.com>
2025-11-11 22:49:45 +0900

    feat: Update schema.sql (binary_contents)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,9 +17,11 @@ CREATE TABLE binary_contents
 (
     id           UUID PRIMARY KEY,
     created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at   timestamp with time zone,
     file_name    VARCHAR(255)             NOT NULL,
     size         BIGINT                   NOT NULL,
-    content_type VARCHAR(100)             NOT NULL
+    content_type VARCHAR(100)             NOT NULL,
+    status       varchar(20)              NOT NULL
 --     ,bytes        BYTEA                    NOT NULL
 );
 
