c898417b5abcb4a04c420761e714e736bd7887df
okodee <okodee@naver.com>
2025-11-11 22:45:59 +0900

    feat: Add BinaryContentStatus Column to BinaryContent

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -3,6 +3,8 @@ package com.sprint.mission.discodeit.entity;
 import com.sprint.mission.discodeit.entity.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import java.util.Objects;
 import lombok.AccessLevel;
@@ -22,6 +24,10 @@ public class BinaryContent extends BaseEntity {
 
     @Column(name = "content_type", nullable = false, length = 100)
     private String contentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BinaryContentStatus status = BinaryContentStatus.PROCESSING;
 
     public BinaryContent(String fileName, Long size, String contentType) {
         this.fileName = Objects.requireNonNull(fileName, "File name must not be null");
