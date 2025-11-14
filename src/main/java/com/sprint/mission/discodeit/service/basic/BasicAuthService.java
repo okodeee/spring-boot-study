2be56798a5669ee4e86b797d9a1ff5524512965f
okodee <okodee@naver.com>
2025-11-14 14:00:12 +0900

    feat: Publish an event when Role is updated

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -6,6 +6,7 @@ import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.event.message.RoleUpdatedEvent;
 import com.sprint.mission.discodeit.exception.DiscodeitException;
 import com.sprint.mission.discodeit.exception.ErrorCode;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
@@ -18,6 +19,7 @@ import com.sprint.mission.discodeit.service.AuthService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +35,7 @@ public class BasicAuthService implements AuthService {
     private final JwtRegistry jwtRegistry;
     private final JwtTokenProvider tokenProvider;
     private final UserDetailsService userDetailsService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @PreAuthorize("hasRole('ADMIN')")
     @Transactional
@@ -48,10 +51,14 @@ public class BasicAuthService implements AuthService {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> UserNotFoundException.withId(userId));
 
+        Role previousRole = user.getRole();
         Role newRole = request.newRole();
         user.updateRole(newRole);
 
         jwtRegistry.invalidateJwtInformationByUserId(userId);
+        eventPublisher.publishEvent(
+            new RoleUpdatedEvent(user.getId(), previousRole, newRole, user.getUpdatedAt())
+        );
 
         return userMapper.toDto(user);
     }
