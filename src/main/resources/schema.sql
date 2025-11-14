9b83d5ce04001cebfd0bcca74f0ad28651097489
okodee <okodee@naver.com>
2025-11-14 14:21:25 +0900

    feat: Update schema.sql (notifications)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -68,6 +68,16 @@ CREATE TABLE read_statuses
     UNIQUE (user_id, channel_id)
 );
 
+-- Notification
+CREATE TABLE notifications
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
+    receiver_id uuid                     NOT NULL,
+    title       varchar(255)             NOT NULL,
+    content     text                     NOT NULL
+);
 
 -- 제약 조건
 -- User (1) -> BinaryContent (1)
